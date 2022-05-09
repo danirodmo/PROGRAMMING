350e2feb9675196d977836a30c5707d31252886d
danirodmo <danirodmo@gmail.com>
2022-05-09 18:29:23 -0500

    Terminado número perfecto

--- PYTHON/numeroPerfecto.py
+++ PYTHON/numeroPerfecto.py
@@ -1,11 +1,14 @@
-numero=int(input("Ingrese un número: "))
-sumaDivisores=0
-for i in range (1,numero):
-    if numero%i==0:
-        sumaDivisores=i+sumaDivisores
-        print(f"Sus divisores son: {i}")
-print(sumaDivisores)
-if sumaDivisores==numero:
-    print(f"{numero} es número perfecto")
-else:
-    print(f"{numero} no es número perfecto")
+#numero=0 #para trabajar con while
+for numero in range(1000+1):
+    #numero=int(input("Ingrese un número: ")) pidiendo que se pregunta el numero
+    sumaDivisores=0
+    for i in range (1,numero):
+        if numero%i==0:
+            sumaDivisores=i+sumaDivisores
+    
+    if sumaDivisores==numero:
+        print(f"{numero} es número perfecto")
+        
+    else:
+        print(f"{numero} no es número perfecto")
+    #numero=numero+1 #para trabajar con while
