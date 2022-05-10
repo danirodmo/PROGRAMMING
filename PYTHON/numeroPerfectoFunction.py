49c0cf14d81860a79b01961083ef6fc7205add4d
danirodmo <danirodmo@gmail.com>
2022-05-10 16:01:33 -0500

    Adicionado numero perfecto como función

---
+++ PYTHON/numeroPerfectoFunction.py
@@ -0,0 +1,16 @@
+#numero=0 #para trabajar con while
+
+def numPerfect ():
+    for numero in range(1000+1):
+        #numero=int(input("Ingrese un número: ")) pidiendo que se pregunta el numero
+        sumaDivisores=0
+        for i in range (1,numero):
+            if numero%i==0:
+                sumaDivisores=i+sumaDivisores
+        
+        if sumaDivisores==numero:
+            print(f"{numero} es número perfecto")
+            
+        #else:
+        #   print(f"{numero} no es número perfecto")
+        #numero=numero+1 #para trabajar con while
