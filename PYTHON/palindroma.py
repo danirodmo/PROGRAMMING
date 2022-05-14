068ffe987eaea5c4936aa28a3c4de3c357f424cd
danirodmo <danirodmo@gmail.com>
2022-05-14 16:50:51 -0500

    Palíndromo perfeccionado

--- PYTHON/palindroma.py
+++ PYTHON/palindroma.py
@@ -3,28 +3,28 @@ from dataclasses import replace
 def palindroma(fraseOr):
     frase=fraseOr.lower().replace(" ","").replace("á","a").replace("é","e").replace("í","i").replace("ó","o").replace("ú","u")
     long=len(frase)//2
-    print(long)
     for n in range (long):
         if frase[n]!=frase[len(frase)-1-n]:
-            Valor=False
+            esPalindroma=False
             break
         else:
-            Valor=True
+            esPalindroma=True
 
-    if Valor==True:    
+    if esPalindroma==True:    
             print(f"La frase {fraseOr} es palíndroma")
-            #print(frase[n],frase[len(frase)-1-n])
     else:
         print(f"La frase {fraseOr} no es palíndroma")
     
-palindroma("Sé verlos al revés")
-            
-#DábalE arroz a la zorra el abad
-#Anita lava la tina
-#Somos o no somos
-#Isaac no ronca así
-#Sé verlas al revés
-#Amó la paloma
-#Luz azul
-#Yo hago yoga hoy
-#Ana lava lana
+palindroma("Yo haGo Yogá hoy")
+
+'''FRASES PALÍNDROMAS:            
+DábalE arroz a la zorra el abad
+Anita lava la tina
+Somos o no somos
+Isaac no ronca así
+Sé verlas al revés
+Amó la paloma
+Luz azul
+Yo hago yoga hoy
+Ana lava lana
+'''
