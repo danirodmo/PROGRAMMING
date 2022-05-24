d65b5719316b38015d8ef5b10baa758eb86c316f
danirodmo <danirodmo@gmail.com>
2022-05-23 20:31:35 -0500

    reto 5 mal

--- PYTHON/Reto5.py
+++ PYTHON/Reto5.py
@@ -1,22 +1,28 @@
 import json
-{
-    "NOMBRE_EMPRESA": "ServiPaquetes",
-    "PAGINA_WEB": "www.nuwebs.com.co",
-    "PAQUETES": [
-        {
-            "ALTO": 10,
-            "ANCHO": 15,
-            "PROFUNDO": 20
-        },
-        {
-            "ALTO": 30,
-            "ANCHO": 10,
-            "PROFUNDO": 10
-        },
-        {
-            "ALTO": 5,
-            "ANCHO": 15,
-            "PROFUNDO": 7
-        }
-    ]
-}
+with open("C:/Users/drodriguez/Downloads/paquetes.json") as file:
+    empresa = json.load(file)
+
+print((len(empresa['PAQUETES'])))
+
+
+#exit()
+def calcularCosto(alto,ancho,profundo):
+    volumen=alto*ancho*profundo
+    if alto<=30:
+        costoEnvio=5*volumen
+    if alto>30:
+        costoEnvio=5*volumen+2000
+    if costoEnvio>10000:
+        costoEnvio=costoEnvio*1.19
+    return costoEnvio
+
+def costoTotal(listaPaquetes,descuento):
+    Total=0
+    for paquete in range (len(listaPaquetes)):
+        alto=empresa["PAQUETES"][paquete]["ALTO"]
+        ancho=empresa["PAQUETES"][paquete]["ANCHO"]
+        profundo=empresa["PAQUETES"][paquete]["PROFUNDO"]
+        Total=(Total+calcularCosto(alto,ancho,profundo))
+    return Total*(1-descuento/100)
+
+print (costoTotal(empresa['PAQUETES'], 10))
