b2aba7bef6ebca8cbedbf90d0f0e3f2d84639828
danirodmo <danirodmo@gmail.com>
2022-06-15 07:40:17 -0500

    Python a Java

---
+++ JAVA/clase1/src/main/java/com/misiontic/clase1/Clase1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.misiontic.clase1;
+
+/**
+ *
+ * @author Daniel Rodriguez
+ */
+public class Clase1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! My first java program");
+        
+    }
+}
